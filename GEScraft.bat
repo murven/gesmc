6db63b4a7204c1d939551acca82615d03c199a57
DevOpsCow <hthiele@gmail.com>
2018-04-30 09:33:47 -0600

    Update GEScraft.bat
    A bit of error handling for the `cd` and `git pull` commands.

--- GEScraft.bat
+++ GEScraft.bat
@@ -1,4 +1,9 @@
 @echo off
 cd %appdata%\.minecraft && git pull
+if errorlevel 1 (
+   echo 'Error pulliando el git!'
+   echo 'Revise el folder de MC y que git este clonado correctamente.'
+   exit /b %errorlevel%
+)
 echo "Moo!"
 pause
